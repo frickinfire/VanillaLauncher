bf1a3c63e4b3ce2a81e7bdf870204a94cb1f6873
fire <destiny187yt@gmail.com>
2024-02-27 15:04:59 -0600

    inital setup for bodycolors

---
+++ VanillaLauncher/Classes/ColorLoader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VanillaLauncher.Classes
+{
+    class ColorLoader
+    {
+        void loadFile()
+        {
+            var filepath = "files\\colors.txt";
+        }
+
+    }
+}
