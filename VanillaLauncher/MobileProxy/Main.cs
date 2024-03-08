2ceeb0d84d2dd89ec489344eea9f0a12bbd7a771
fire <fire@epicgamers.xyz>
2024-03-07 20:21:24 -0600

    target .NET 4.5.2 and add proxy stuff

---
+++ VanillaLauncher/MobileProxy/Main.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Security;
+using System.Threading;
+using System.Threading.Tasks;
+using Titanium.Web.Proxy.EventArguments;
+using Titanium.Web.Proxy.Exceptions;
+using Titanium.Web.Proxy.Helpers;
+using Titanium.Web.Proxy.Http;
+using Titanium.Web.Proxy.Models;
+using Titanium.Web.Proxy.StreamExtended.Network;
+using System.Security.Cryptography.X509Certificates;
+
+namespace VanillaLauncher.MobileProxy
+{
+    class Main
+    {
+        void Start()
+        {
+        }
+
+    }
+}
